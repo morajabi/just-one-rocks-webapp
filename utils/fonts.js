233d1fd90dac67454cf783a08731acb3578538f7
Mohammad Rajabifard <mo.rajbi@gmail.com>
2017-09-15 21:25:34 +0430

    fix(font): use correct fonts

---
+++ utils/fonts.js
@@ -0,0 +1,2 @@
+export const bodyFont = '-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
+export const headerFont = `"Avenir Next", ${bodyFont}`
