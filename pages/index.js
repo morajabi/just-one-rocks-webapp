11717ced7ef573dee32e90af7d87c8fe6ee3ec69
Mohammad Rajabifard <mo.rajbi@gmail.com>
2017-09-15 21:19:52 +0430

    Add index and _document

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Heading = styled.h1`
+  color: red;
+`
+
+const Index = () => <Heading>Hi</Heading>
+
+export default Index
