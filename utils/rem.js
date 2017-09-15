79285d291667134ea16bf7f14035055e04885891
Mohammad Rajabifard <mo.rajbi@gmail.com>
2017-09-15 21:28:10 +0430

    feat: add rem util

---
+++ utils/rem.js
@@ -0,0 +1,6 @@
+// https://github.com/styled-components/styled-components-website/blob/master/utils/rem.js
+import rem from 'polished/lib/helpers/rem'
+
+const _rem = size => rem(size, '18px')
+
+export default _rem
