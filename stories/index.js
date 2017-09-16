f7d7078f800c972a50ed311395a6907c464c4fd1
Behnam <b.rajabifard@gmail.com>
2017-09-16 14:20:42 +0430

    add Cabin font

--- stories/index.js
+++ stories/index.js
@@ -6,8 +6,13 @@ import { linkTo } from '@storybook/addon-links'
 
 import { Button, Welcome } from '@storybook/react/demo'
 
+import ShareThis from '../components/ShareThis'
+
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />)
 
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
   .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>)
+
+storiesOf('ShareThis', module)
+  .add('Full', () => <ShareThis />)
