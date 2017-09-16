51533acf600dbd548b25e24dd77433315c0df627
Behnam <b.rajabifard@gmail.com>
2017-09-16 23:57:51 +0430

    add: title add to karmabox

--- components/KarmaBox.js
+++ components/KarmaBox.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
-const KrmaTitle = styled.h2`
-  color: #000;
+const KarmaContainer = styled.div`
+  background: rgba(0, 0, 0, 0.02);
+  font-family: 'Cabin';
+  overflow: hidden;
+`
+const KarmaTitle = styled.h2`
+  font-weight: 700;
 `
 
 export default class KarmaBox extends Component {
-  reander() {
+  render() {
     return (
-      <KrmaTitle>Your Karma IS 120</KrmaTitle>
+      <KarmaContainer>
+        <KarmaTitle>Yes! You have</KarmaTitle>
+        <div>
+          
+        </div>
+      </KarmaContainer>
     )
   }
 }
