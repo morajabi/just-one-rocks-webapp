1fb122f7304fca6fd254dfbf2ce75858ad22ea74
Behnam <b.rajabifard@gmail.com>
2017-09-16 16:13:16 +0430

    style: add new styles

--- components/ShareThis.js
+++ components/ShareThis.js
@@ -5,13 +5,15 @@ const ShareThisCountainer = styled.div`
   width: 262px;
   height: 169px;
   background: rgba(0, 0, 0, 0.02);
+  font-family: 'Cabin';
+  font-style: normal;
 `
 
 export default class ShareThis extends Component {
   render(){
     return (
       <ShareThisCountainer>
-        Share This
+        Share this
       </ShareThisCountainer>
     )
   }
