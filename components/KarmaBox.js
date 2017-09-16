39e16af85ceb09b9a4e0cbd60d0b22bf574fa962
Behnam <b.rajabifard@gmail.com>
2017-09-17 00:21:38 +0430

    fix: change all names without karma

--- components/KarmaBox.js
+++ components/KarmaBox.js
@@ -1,24 +1,44 @@
-import React, { Component } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 
-const KarmaContainer = styled.div`
+import rem from '../utils/rem'
+
+const Container = styled.div`
   background: rgba(0, 0, 0, 0.02);
   font-family: 'Cabin';
   overflow: hidden;
 `
-const KarmaTitle = styled.h2`
+const Title = styled.h2`
   font-weight: 700;
 `
 
-export default class KarmaBox extends Component {
-  render() {
-    return (
-      <KarmaContainer>
-        <KarmaTitle>Yes! You have</KarmaTitle>
-        <div>
-          
-        </div>
-      </KarmaContainer>
-    )
-  }
-}
+const ScoreContaner = styled.div`
+  display: flex;
+`
+
+const Score = styled.div`
+  flex: 0 1 auto;
+  font-size: ${rem(34)};
+  border: 1px solid #999;
+`
+
+const Share = styled.div`
+  flex: 1 0 auto;
+  margin-left: ${rem(5)};
+`
+
+const KarmaBox = () => (
+  <Container>
+    <Title>Yes! You have</Title>
+    <ScoreContaner>
+      <Score>
+        18
+      </Score>
+      <Share>
+        SHare
+      </Share>
+    </ScoreContaner>
+  </Container>
+)
+
+export default KarmaBox
