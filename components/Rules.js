45d3f4b8efe7dab7e2a1952771e7328002738707
Behnam Rajabifard <b.rajabifard@gmail.com>
2017-09-18 00:51:59 +0430

    Rules (#5)
    * fix: remove unessery import
    
    * add: rules component
    
    * add: rules components in storybook
    
    * add: new rules & items
    
    * fix: width problem
    
    * fix: compelet rules componet

---
+++ components/Rules.js
@@ -0,0 +1,66 @@
+import styled from 'styled-components'
+
+import rem from '../utils/rem'
+
+const Countainer = styled.div`
+  font-family: 'Avenir Next';
+  width: 100%;
+  font-size: ${rem(30)};
+  color: #555;
+`
+const Title = styled.h2`
+  font-size: ${rem(30)};
+  font-weight: 700;
+  color: #555;
+  margin: 0;
+`
+const SubTitle = styled.p`
+  margin: ${rem(12)} 0 0;
+  font-size: ${rem(16)};
+  color: #555;
+  font-weight: 400;
+`
+const RuleList = styled.ul`
+  margin: ${rem(20)} 0 0;
+  padding: 0;
+  font-size: ${rem(16)};
+  color: #555;
+  font-weight: 400;
+  line-height: 1.44;
+`
+const Item = styled.li`
+  position: relative;
+  padding-left: ${rem(24)};
+  margin: 0;
+  margin-bottom: ${rem(20)};
+  font-size: ${rem(16)};
+  color: #555;
+
+  &:before {
+    content: ' ';
+    position: absolute;
+    left: 0;
+    top: ${rem(7)};
+    width: ${rem(8)};
+    height: ${rem(8)};
+    background: rgba(0, 0, 0, 0.15);
+    font-size: 30px;
+    border-radius: 50%;
+  }
+`
+
+const Rules = () => (
+  <Countainer>
+    <Title>Rules</Title>
+    <SubTitle>There are some rules to keep everyone 😊</SubTitle>
+
+    <RuleList>
+      <Item>You should only say one pro or con in every message (there’s no such rule for replies)</Item>
+      <Item>Don’t make any one unhappy. We want to have fun here!</Item>
+      <Item>You should only say one pro or con in every message (there’s no such rule for replies)</Item>
+      <Item>Don’t make any one unhappy. We want to have fun here!</Item>
+    </RuleList>
+  </Countainer>
+)
+
+export default Rules
