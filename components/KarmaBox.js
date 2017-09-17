100c464604fb618cbba6f809bd32e70871461990
Behnam <b.rajabifard@gmail.com>
2017-09-17 14:05:59 +0430

    style: fix all karmabox px to rem

--- components/KarmaBox.js
+++ components/KarmaBox.js
@@ -23,31 +23,33 @@ const Score = styled.div`
 `
 const Share = styled.div`
   flex: 0 1 100%;
-  margin-left: ${rem(10)};
+  margin-left: ${rem(15)};
   overflow: hidden;
 `
 const Karmas = styled.span`
-  font-size: ${rem(45)};
+  font-size: ${rem(46)};
   letter-spacing: 1.8px;
   color: #19abf9;
   display: block;
   font-weight: 700;
 `
 const KarmasText = styled.span`
-  font-size: ${rem(15)};
+  font-size: ${rem(17)};
   color: #19abf9;
   font-weight: 700;
   display: block;
+  padding-left: ${rem(3)};
+  padding-top: ${rem(2)};
 `
 const Help = styled.p`
-  font-size: 15px;
+  font-size: ${rem(18)};
   color: #555;
   white-space: wrap;
-  margin-bottom: 0;
+  margin: ${rem(9)} 0 ${rem(3)};
 `
 const ShareWork = styled.a`
   font-family: Avenir Next;
-  font-size: 11px;
+  font-size: ${rem(11)};
   text-decoration-line: underline;
   color: #76a9ea;
 `
