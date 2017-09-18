1759c570ef8912750fe43585bce0561dd76f0938
Behnam <b.rajabifard@gmail.com>
2017-09-18 14:15:10 +0430

    add: complete all styles

--- components/FilterBar.js
+++ components/FilterBar.js
@@ -1,17 +1,49 @@
 import styled from 'styled-components'
 
+import rem from '../utils/rem'
+
 const Container = styled.div`
+  background: #fcfcfc;
   font-family: 'Assistant';
   width: 100%;
-  background: #fcfcfc;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 ${rem(22)};
 `
 const SortContainer = styled.div`
-  width: 50%;
+  flex: 0 1 auto;
+`
+const Text = styled.p`
+  font-weight: 600;
+  font-size: ${rem(14)};
+  color: #bdbdbd;
+  display: inline-block;
+`
+const FilterTag = styled.span`
+  background: ${props => props.primary ? '#61c8ff' : 'rgba(233, 233, 233, 0.37)'};
+  color: ${props => props.primary ? '#fff' : '#555'};
+  font-weight: 600;
+  font-size: ${rem(14)};
+  text-align: center;
+  border-radius: ${rem(8)};
+  padding: ${rem(3)} ${rem(14)};
+  margin-left: ${rem(10)};
+  line-height: 1;
 `
 
 const FilterBar = () => (
   <Container>
-    <SortContainer>sort by</SortContainer>
+    <SortContainer>
+      <Text>sort by</Text>
+      <FilterTag>most recent</FilterTag>
+      <FilterTag primary>most popular</FilterTag>
+    </SortContainer>
+    <SortContainer>
+      <Text>filter</Text>
+      <FilterTag>pros</FilterTag>
+      <FilterTag>cons</FilterTag>
+    </SortContainer>
   </Container>
 )
 
