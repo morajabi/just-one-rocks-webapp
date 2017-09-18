df9ad8fbb7c03a9076fd2a9cbea4ec3903c0edad
Behnam <b.rajabifard@gmail.com>
2017-09-19 01:10:19 +0430

    Add: dot component

---
+++ components/Dot.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+import rem from '../utils/rem'
+
+const Dot = styled.div`
+  width: ${rem(6)};
+  height: ${rem(6)};
+  background: rgba(0, 0, 0, 0.11);
+  font-size: ${rem(30)};
+  border-radius: 50%;
+  display: inline-block;
+`
+export default Dot
