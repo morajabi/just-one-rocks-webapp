c5538be38e38d8a87a7807a1fd77712e244e4fa3
Behnam <b.rajabifard@gmail.com>
2017-09-18 20:32:01 +0430

    add: user and message added

--- components/Message.js
+++ components/Message.js
@@ -1,15 +1,36 @@
 import styled from 'styled-components'
 
 import rem from '../utils/rem'
-import { headerFont } from '../utils/fonts'
+import { messageFont, headerFont } from '../utils/fonts'
 
 const Container = styled.div`
   font-family: ${headerFont};
-  width: ${rem(100)};
+  display: flex;
+  font-size: ${rem(20)};
 `
-
+const UserInfo = styled.div`
+  flex: 0 1 auto;
+`
+const MessageContent = styled.div`
+  flex: 0 1 100%;
+  font-family: ${messageFont};
+`
+const UserAvatar = styled.div`
+  background: #000;
+  width: ${rem(32)};
+  height: ${rem(32)};
+`
 const Message = () => (
-  <Container>Hello World</Container>
+  <Container>
+    <UserInfo>
+      <UserAvatar>
+
+      </UserAvatar>
+    </UserInfo>
+    <MessageContent>
+      <MessageContent>Message Content</MessageContent>
+    </MessageContent>
+  </Container>
 )
 
 export default Message
