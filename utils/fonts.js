cb6ba0b2aef46b497ae0e713cbbe86edcc6b366c
Behnam <b.rajabifard@gmail.com>
2017-09-19 12:17:32 +0430

    fix: remove message font

--- utils/fonts.js
+++ utils/fonts.js
@@ -1,3 +1,2 @@
-export const bodyFont = '-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
+export const bodyFont = 'Roboto, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
 export const headerFont = `"Assistant", ${bodyFont}`
-export const messageFont = `"Roboto", ${bodyFont}`
