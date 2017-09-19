d76b559d25e97a2e6c888674bfc8414dc306b977
Behnam <b.rajabifard@gmail.com>
2017-09-19 19:59:55 +0430

    add: new tests for Replay bubble

--- stories/index.js
+++ stories/index.js
@@ -32,11 +32,55 @@ storiesOf('Header', module)
 storiesOf('Message', module)
   .add('Message', () => <div style={{ width: 571 }}><Message /></div>)
   
-storiesOf('Chat', module)
-  .add('ReplyBubble', () => 
+storiesOf('ReplyBubble', module)
+  .add('RB start', () => 
       <div style={{ width: 355 }}>
         <ReplyBubble 
-        userPicture=""
+          align="end"
+          content="Hi!"
         />
       </div>
-    )
+  )
+  .add('RB end', () => 
+      <div style={{ width: 355 }}>
+        <ReplyBubble 
+          align="start"
+          content="Hi!"
+        />
+      </div>
+  )
+  .add('RB style & content', () => 
+      <div style={{ width: 355 }}>
+        <ReplyBubble 
+          align="start"
+          bubbleStyle="primary"
+          userColor="#0f0"
+          content="Hi!"
+          upVoteCount="12"
+        />
+      </div>
+  )
+  .add('RB with UpVote', () => 
+      <div style={{ width: 355 }}>
+        <ReplyBubble 
+          align="start"
+          bubbleStyle="primary"
+          userColor="#0f0"
+          content="Hi!"
+          upVoteCount="12"
+          onUpVote={ action('Upvote')}
+        />
+      </div>
+  )
+  .add('RB with multiline', () => 
+      <div style={{ width: 355 }}>
+        <ReplyBubble 
+          align="start"
+          bubbleStyle="primary"
+          userColor="#0f0"
+          content="Hi h s lkasdjlk asjdkla jskdsaj dlkdkladlklkas asdjaks jkldsaj klasas dashkjdasj dlkajsl kdjasld!"
+          upVoteCount="12"
+          onUpVote={ action('clicked') }
+        />
+      </div>
+  )
