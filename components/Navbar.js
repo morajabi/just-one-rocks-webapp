a2cfbad4f7d80e8dbdab7a264b9238564935d3d3
Behnam <b.rajabifard@gmail.com>
2017-09-20 12:53:13 +0430

    fix: split header

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,36 @@
-import { styled } from 'styled-components'
+import styled from 'styled-components'
 
-import { rem } from '../utils/rem'
+import rem from '../utils/rem'
 
 const Container = styled.div`
   width: 100%;
+  display: flex;
+  justify-content: space-between;
+`
+const LogoTitleContainer = styled.div`
+  width: auto;
 `
 const LogoContainer = styled.div`
   width: ${rem(180)};
   height: ${rem(95)};
   background: #efa600;
+  display: inline-block;
+`
+const Title = styled.div`
+  font-size: ${rem(22)};
+  color: #555;
+  display: inline-block;
+`
+const Nav = styled.nav`
+  flex: 0 1 auto;
 `
 const Navbar = () => (
   <Container>
-    <LogoContainer></LogoContainer>
+    <LogoTitleContainer>
+      <LogoContainer>LOGO</LogoContainer>
+      <Title>Help others to find the better option</Title>
+    </LogoTitleContainer>
+    <Nav>log in . register</Nav>
   </Container>
 )
 
