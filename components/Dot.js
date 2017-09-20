81ae6ce207607350a012bf1a4a9d82caa96adc09
Behnam <b.rajabifard@gmail.com>
2017-09-20 14:14:38 +0430

    fix: rem function

--- components/Dot.js
+++ components/Dot.js
@@ -3,8 +3,8 @@ import styled from 'styled-components'
 import rem from '../utils/rem'
 
 const Dot = styled.div`
-  width: ${ p => rem(p.size) || rem(6) };
-  height: ${ p => rem(p.size) || rem(6) };
+  width: ${p => rem(p.size || 6)};
+  height: ${p => rem(p.size || 6)};
   background: rgba(0, 0, 0, 0.11);
   font-size: ${rem(30)};
   border-radius: 50%;
