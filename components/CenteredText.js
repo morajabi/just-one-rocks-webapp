cfc02a4e5d8f153183ee5d7c6be381f13b87ea12
Mohammad Rajabifard <mo.rajbi@gmail.com>
2017-10-06 16:39:15 +0330

    feat: add CenteredText component

---
+++ components/CenteredText.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const CenteredText = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export default CenteredText
